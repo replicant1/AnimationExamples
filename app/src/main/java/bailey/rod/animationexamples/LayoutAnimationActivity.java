269c6641afb8c1fa2e83c54554057a8e1be99018
rodbailey <rodney.p.bailey@gmail.com>
2016-07-27 19:04:34 +1000

    Initial commit

---
+++ app/src/main/java/bailey/rod/animationexamples/LayoutAnimationActivity.java
@@ -0,0 +1,18 @@
+package bailey.rod.animationexamples;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rodbailey on 27/07/2016.
+ */
+public class LayoutAnimationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_animation);
+    }
+}
